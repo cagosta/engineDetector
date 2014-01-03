5a9fcf4dc10d8544165ae33f44c38670c3376172
cyril agosta <cyril.agosta@gmail.com>
2014-01-03 14:22:43 +0100

    Releasing v0.0.4

--- dist/build/main.js
+++ dist/build/main.js
@@ -496,7 +496,7 @@ define( 'engineDetector/EngineDetector',[],function() {
 
 } );
 /**
- * engineDetector version: "0.0.3" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
+ * engineDetector version: "0.0.4" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
  * Available via the MIT license.
  * see: http://github.com/cagosta/engineDetector for details
  */
